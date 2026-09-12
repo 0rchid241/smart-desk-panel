0f13d70144a0b2907d562e52f9637a521839ece4
dlehgns02 <dlehgns02@naver.com>
2026-09-12 20:43:04 +0900

    ESP32 OLED 화면 출력 테스트 추가

--- firmware/smart_desk_esp32/smart_desk_esp32.ino/smart_desk_esp32.ino.ino
+++ firmware/smart_desk_esp32/smart_desk_esp32.ino/smart_desk_esp32.ino.ino
@@ -1,12 +1,41 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(
+  SCREEN_WIDTH,
+  SCREEN_HEIGHT,
+  &Wire,
+  OLED_RESET
+);
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
 
+  Wire.begin(21, 22);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println("OLED init failed");
+    while (true);
+  }
+
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+
+  display.setCursor(0, 0);
+  display.println("SMART DESK");
+  display.println();
+  display.println("OLED TEST OK");
+
+  display.display();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello Smart Desk");
-  delay(1000);
-
 }
